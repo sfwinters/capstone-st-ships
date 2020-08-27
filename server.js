a3a09d326bc943267aa71f19c431def13064f995
sfwinters <sarah.f.winters@gmail.com>
2020-08-27 19:27:18 -0400

    installed Express and modified server.js file again

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname, './dist/capstone-st-ships'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/capstone-st-ships/index.html'));
+    res.sendFile('index.html', { root: 'dist/capstone-st-ships' });
 });
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
