12b09725bd9bcf55c03c08cafcc150973d92989c
sfwinters <sarah.f.winters@gmail.com>
2020-08-27 16:11:16 -0400

    trying another fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/capstone-st-ships'));
+app.use(express.static(__dirname + '/dist/capstone-to-heroku'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/capstone-st-ships/index.html'));
+res.sendFile(path.join(__dirname+'/dist/capstone-to-heroku/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
