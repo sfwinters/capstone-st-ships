f43d5f60084b53279e4443f3ac5027df0bec2bd2
sfwinters <sarah.f.winters@gmail.com>
2020-08-27 19:11:20 -0400

    rewrote server.js file

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
 
-app.use(express.static(__dirname, 'dist', {index:false}));
+const app = express();
 
-server.listen(port, function(){
-    console.log("App running on port " + port);
-})
+app.use(express.static(__dirname, './dist/capstone-st-ships'));
 
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist/capstone-st-ships/index.html'));
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+app.listen(process.env.PORT || 8080);
