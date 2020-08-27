40838495898331e0da4fb71556648bcb4c6f36bc
sfwinters <sarah.f.winters@gmail.com>
2020-08-27 19:42:08 -0400

    changed folder name

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname, './dist/capstone-st-ships'));
+app.use(express.static(__dirname, './dist/capstone-st-ships/src/'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/capstone-st-ships' });
+    res.sendFile('index.html', { root: 'dist/capstone-st-ships/src/' });
 });
 
 app.listen(process.env.PORT || 8080);
