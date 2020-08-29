260d28b8a3021f37dc14ae8edb3bb994fd0f0b43
sfwinters <sarah.f.winters@gmail.com>
2020-08-29 12:30:48 -0400

    trying to get font to render on klingon page

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/*', function(req, res) {
 
 app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`)
+// console.log(`Running on port ${process.env.PORT || 8080}`)
