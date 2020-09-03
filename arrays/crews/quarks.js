adf3b95c5777af6faa2e0890db4a8afc44215d20
sfwinters <sarah.f.winters@gmail.com>
2020-09-02 22:33:54 -0400

    docs

--- arrays/crews/quarks.js
+++
@@ -1,7 +0,0 @@
-const quark = {
-    'Quark\'s Treasure': owner
-}
-
-const rom = {
-    occupation: ['Diagnostic and Repair Technician', 'Grand Nagus']
-}
--- arrays/fleets/borg.js
+++
@@ -1,9 +0,0 @@
-class Starship {
-    constructor(type, use) {
-        this.type = type,
-        this.use = use
-    }
-}
-
-const sphere = new Starship('Borg sphere', 'Scout ship');
-const cube = new Starship('Borg cube', 'Assimilation')
